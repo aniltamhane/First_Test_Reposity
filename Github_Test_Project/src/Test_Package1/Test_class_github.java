8692c6e2239701f690c8853479fe957a57467ba5
Anilt <anil.tamhane@gmail.com>
2017-01-25 11:46:02 +0530

    First_checkin_Test_Project

---
+++ Github_Test_Project/src/Test_Package1/Test_class_github.java
@@ -0,0 +1,22 @@
+package Test_Package1;
+
+public class Test_class_github {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=4,sum=0;
+		
+		for(int i=0;i<=a;i++){
+			sum=sum+i;
+			System.out.println(sum);
+		}
+		
+		
+
+	}
+
+}
