a2206ef3b613b3cc511c7e69be2e24ca9339f91f
Anilt <anil.tamhane@gmail.com>
2017-01-25 11:59:42 +0530

    Added palindrome number programe

--- Github_Test_Project/src/Test_Package1/Test_class_github.java
+++ Github_Test_Project/src/Test_Package1/Test_class_github.java
@@ -1,6 +1,30 @@
 package Test_Package1;
 
+
+
 public class Test_class_github {
+	
+	
+	public void palindrome_number(){
+		int r,sum=0,temp;    
+		  int n=102; //It is the number variable to be checked for palindrome  
+		  
+		  temp=n;    
+		  while(n>0){    
+		   r=n%10;  //getting remainder  
+		   sum=(sum*10)+r;    
+		   n=n/10;    
+		   
+		  if(temp==sum) {   
+		   System.out.println("Palindrome number ");
+		  }
+		  else    
+		  {
+			  System.out.println("Not palindrome");    
+		  }
+		 }
+		
+	} 
 
 	/**
 	 * @param args
@@ -15,8 +39,5 @@ public class Test_class_github {
 			System.out.println(sum);
 		}
 		
-		
-
 	}
-
 }
