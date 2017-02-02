eddf4428ae6d9b3c6283fb7a6153aaf8e50fbf82
Anilt <anil.tamhane@gmail.com>
2017-02-02 15:31:33 +0530

    Program for fibonacci number.

---
+++ Github_Test_Project/src/Test_Package1/fibonacci_number.java
@@ -0,0 +1,23 @@
+package Test_Package1;
+
+public class fibonacci_number {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int count=10, num1=0, num2=1,num3=0;
+		System.out.println(num1);
+		for (int i=0;i<count;i++){
+			num3= num1+num2;
+			System.out.println(num3);
+			num1=num2;
+			num2=num3;
+		}
+		
+		
+	}
+
+}
