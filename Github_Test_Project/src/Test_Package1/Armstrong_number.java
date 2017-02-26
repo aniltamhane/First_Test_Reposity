4d5f817c04e409dbd5a6bf17dd2e6b9e5a24491c
Anil <anil.tamhane@gmail.com>
2017-02-26 11:48:38 +0530

    Programme for armstrong number.

---
+++ Github_Test_Project/src/Test_Package1/Armstrong_number.java
@@ -0,0 +1,30 @@
+package Test_Package1;
+
+import java.util.Scanner;
+
+public class Armstrong_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input1 = new Scanner(System.in);
+		System.out.println("Please enter number: ");
+		int num = input1.nextInt();
+		
+		int r, sum = 0, temp=num;
+		
+		while (num > 0){
+			r = num%10;
+			sum = sum + ( r*r*r);
+			num = num/10;
+		}
+		
+		if (sum == temp){
+			System.out.println("Number is armstrong number");
+		}
+		else{
+			System.out.println("Not an armstrong number");
+		}
+	}
+
+}
